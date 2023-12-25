faba1277c4b10f322595b36076390bd1bf3a2e86
Berkay Özgün <berkayozgun001@gmail.com>
2023-12-25 12:05:41 +0300

    feature/comment and vote models and endpoints created, models files are merged into models.py (#4)
    * feat :user and posts model created and endpoints are connected, login and register works
    
    * fix : login & register is working properly with restful endpoints
    
    * feat : post feature is created and postmodel.py is upgraded for mongoengine
    
    * feat : endpoints are protected, votemodel and commentmodel created and all models are merged in models.py

---
+++ models/models.py
@@ -0,0 +1,106 @@
+#imports
+from mongoengine import Document, StringField, DateTimeField, ListField, ReferenceField
+from datetime import datetime, timedelta
+from flask_bcrypt import Bcrypt
+import jwt
+from config import SECRET_KEY
+
+bcrypt = Bcrypt()
+
+# yorum modeli
+class CommentModel(Document):
+    user = StringField(required=True)
+    comment = StringField(required=True)
+    date = DateTimeField(default=datetime.utcnow)
+
+    @property
+    def id(self):
+        return str(self.pk)
+
+    def to_dict(self):
+        return {
+            "user": self.user,
+            "comment": self.comment,
+            "date": self.date.strftime("%Y-%m-%d %H:%M:%S"),
+        }
+
+# kullanıcı modeli
+class UserModel(Document):
+    username = StringField(required=True, unique=True)
+    email = StringField(required=True, unique=True)
+    password = StringField(required=True)
+    name = StringField(required=True)
+    surname = StringField(required=True)
+    birthdate = StringField(required=True)
+    gender = StringField(required=True)
+    profile_image = StringField(required=True)
+
+    @property
+    def user_id(self):
+        return str(self.id)
+
+    @property
+    def token(self):
+        payload = {
+            "user_id": str(self.id),
+            "exp": datetime.utcnow() + timedelta(days=1),
+        }
+        return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+    def to_dict(self):
+        return {
+            "_id": str(self.id),
+            "username": self.username,
+            "email": self.email,
+            "password": self.password,
+            "name": self.name,
+            "surname": self.surname,
+            "birthdate": self.birthdate,
+            "gender": self.gender,
+            "profile_image": self.profile_image,
+        }
+
+    def set_password(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password(self, input_password):
+        return bcrypt.check_password_hash(self.password, input_password)
+
+# post modeli
+class PostModel(Document):
+    author = StringField(required=True)
+    date = DateTimeField(default=datetime.utcnow)
+    title = StringField(required=True)
+    content = StringField(required=True)
+    comments = ListField(ReferenceField(CommentModel))
+    votes = ListField(ReferenceField('VoteModel'))
+
+    @property
+    def id(self):
+        return str(self.pk)
+
+    def to_dict(self):
+        comment_list = [comment.to_dict() for comment in self.comments]
+
+        return {
+            "_id": str(self.pk),
+            "author": self.author,
+            "date": self.date.strftime("%Y-%m-%d %H:%M:%S"),
+            "title": self.title,
+            "content": self.content,
+            "comments": comment_list,
+            "votes": [vote.to_dict() for vote in self.votes] if self.votes else [],
+        }
+
+# oylama modeli
+class VoteModel(Document):
+    user = ReferenceField(UserModel, required=True)
+    post = ReferenceField(PostModel, required=True)
+    vote_type = StringField(choices=["upvote", "downvote"], required=True)
+    date = DateTimeField(default=datetime.utcnow)
+
+    def to_dict(self):
+        return {"user": str(self.user.id),  # str(self.user.id) == self.user.user_id (property
+                "vote_type": self.vote_type,
+                "post": str(self.post.id),
+                "date": self.date.strftime("%Y-%m-%d %H:%M:%S")}
